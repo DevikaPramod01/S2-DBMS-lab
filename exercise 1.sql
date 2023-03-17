d6a63de9efc196ca312574009ef4cf7d08355f0c
DevikaPramod01 <115997408+DevikaPramod01@users.noreply.github.com>
2023-03-17 15:29:08 +0530

    Update exercise 1.sql

--- exercise 1.sql
+++ exercise 1.sql
@@ -22,3 +22,17 @@ alter table accident drop location;
 
 alter table participated modify damage_amt numeric(7,2);
 
+insert into person (driver_id,name,address,gender)values("101","akshay","ak house","m"),("202","anusha","an vila","f"),("303","catherine","ca house","f"),("401","kiran","kk nivas","m"),("451","raj","raj nivas","m");
+
+insert into accident(report_no,date)values("4563","2019-11-28"),("5671","2022-09-12"),("8230","2005-03-11"),("9056","2023-07-14"),("5633","2005-05-19");
+
+insert into accident(report_number,date)values("4563","2014-11-28"),("5671","2022-09-12"),("8230","2005-03-11"),("9056","2019-07-14"),("5633","2005-05-19");
+
+insert into owns(driver_id,license)values("101","1001"),("202","6002"),("303","3567"),("401","5008"),("451","7622");
+
+insert into participated (report_no ,license ,driver_id ,damage_amt)values("4563","1001","101","15650.55"),("5671","6002","202","09830.75"),("8230","3567","303","24892.88"),("9056","5008","401","08450.90"),("5633","7622","451","06500.25");
+
+select * from person where name like "a%";
+select * from participated where damage_amt>10000;
+select * from accident where date between "2021-01-01" and "2023-12-31";
+
